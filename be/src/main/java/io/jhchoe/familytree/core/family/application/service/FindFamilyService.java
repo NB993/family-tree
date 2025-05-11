f3f25d9a354430e67ba998c2ebf8c914f9dda4e5
Jaehong Choe <qnalsfj@gmail.com>
2025-05-11 16:20:30 +0900

    feat, test FindFamilyUseCase 분기 처리 추가

--- be/src/main/java/io/jhchoe/familytree/core/family/application/service/FindFamilyService.java
+++ be/src/main/java/io/jhchoe/familytree/core/family/application/service/FindFamilyService.java
@@ -1,5 +1,6 @@
 package io.jhchoe.familytree.core.family.application.service;
 
+import io.jhchoe.familytree.common.exception.FTException;
 import io.jhchoe.familytree.core.family.application.port.in.FindFamilyUseCase;
 import io.jhchoe.familytree.core.family.application.port.out.FindFamilyPort;
 import io.jhchoe.familytree.core.family.domain.Family;
@@ -14,6 +15,11 @@ public class FindFamilyService implements FindFamilyUseCase {
 
     @Override
     public Family findById(Long id) {
-        return findFamilyPort.findById(id).get();
+        if (id == null) {
+            throw FTException.MISSING_PARAMETER;
+        }
+
+        return findFamilyPort.findById(id)
+            .orElseThrow(() -> FTException.NOT_FOUND);
     }
 }
--- be/src/test/java/io/jhchoe/familytree/core/family/application/service/FindFamilyServiceTest.java
+++ be/src/test/java/io/jhchoe/familytree/core/family/application/service/FindFamilyServiceTest.java
@@ -1,12 +1,14 @@
 package io.jhchoe.familytree.core.family.application.service;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.when;
 
+import io.jhchoe.familytree.common.exception.FTException;
 import io.jhchoe.familytree.core.family.application.port.out.FindFamilyPort;
 import io.jhchoe.familytree.core.family.domain.Family;
 import java.time.LocalDateTime;
 import java.util.Optional;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -45,14 +47,43 @@ public class FindFamilyServiceTest {
         // when
         Family family = sut.findById(familyId);
 
-        Assertions.assertThat(family).isNotNull();
-        Assertions.assertThat(family.getId()).isEqualTo(expectedFamily.getId());
-        Assertions.assertThat(family.getName()).isEqualTo(expectedFamily.getName());
-        Assertions.assertThat(family.getDescription()).isEqualTo(expectedFamily.getDescription());
-        Assertions.assertThat(family.getProfileUrl()).isEqualTo(expectedFamily.getProfileUrl());
-        Assertions.assertThat(family.getCreatedBy()).isEqualTo(expectedFamily.getCreatedBy());
-        Assertions.assertThat(family.getCreatedAt()).isEqualTo(expectedFamily.getCreatedAt());
-        Assertions.assertThat(family.getModifiedBy()).isEqualTo(expectedFamily.getModifiedBy());
-        Assertions.assertThat(family.getModifiedAt()).isEqualTo(expectedFamily.getModifiedAt());
+        // then
+        assertThat(family).isNotNull();
+        assertThat(family.getId()).isEqualTo(expectedFamily.getId());
+        assertThat(family.getName()).isEqualTo(expectedFamily.getName());
+        assertThat(family.getDescription()).isEqualTo(expectedFamily.getDescription());
+        assertThat(family.getProfileUrl()).isEqualTo(expectedFamily.getProfileUrl());
+        assertThat(family.getCreatedBy()).isEqualTo(expectedFamily.getCreatedBy());
+        assertThat(family.getCreatedAt()).isEqualTo(expectedFamily.getCreatedAt());
+        assertThat(family.getModifiedBy()).isEqualTo(expectedFamily.getModifiedBy());
+        assertThat(family.getModifiedAt()).isEqualTo(expectedFamily.getModifiedAt());
+    }
+
+    @Test
+    @DisplayName("find 메서드는 id가 null이면 예외를 발생시켜야 한다.")
+    void given_null_id_when_find_ById_then_throw_exception() {
+        // given
+        Long familyId = null;
+
+        // when & then
+        assertThatThrownBy(() -> sut.findById(familyId))
+            .isInstanceOf(FTException.class)
+            .hasMessage("파라미터 누락.");
+    }
+
+    @Test
+    @DisplayName("find 메서드는 id와 일치하는 Family를 찾지 못한 경우 예외를 발생시켜야 한다.")
+    void given_invalid_id_when_find_ById_then_throw_exception() {
+        // given
+        Long notSavedFamilyId = 999L;
+
+        // when
+        when(findFamilyPort.findById(notSavedFamilyId))
+            .thenReturn(Optional.empty());
+
+        // then
+        assertThatThrownBy(() -> sut.findById(notSavedFamilyId))
+            .isInstanceOf(FTException.class)
+            .hasMessage("대상을 찾지 못했습니다.");
     }
 }
