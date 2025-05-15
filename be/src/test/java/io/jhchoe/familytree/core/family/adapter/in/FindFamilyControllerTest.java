e6c4e193dd696bb1056f591e2b92f3ac2a90e8f3
Jaehong Choe <qnalsfj@gmail.com>
2025-05-15 23:35:35 +0900

    test FindFamilyControllerTest 수정
    검증문을 테스트할 때마다 다르게 사용하는 게 좀 거슬림

--- be/src/test/java/io/jhchoe/familytree/core/family/adapter/in/FindFamilyControllerTest.java
+++ be/src/test/java/io/jhchoe/familytree/core/family/adapter/in/FindFamilyControllerTest.java
@@ -7,11 +7,11 @@ import static org.hamcrest.Matchers.notNullValue;
 
 import io.jhchoe.familytree.config.WithMockOAuth2User;
 import io.jhchoe.familytree.core.family.adapter.in.response.FindFamilyResponse;
-import io.jhchoe.familytree.core.family.adapter.out.persistence.FamilyEntity;
+import io.jhchoe.familytree.core.family.adapter.out.persistence.FamilyJpaEntity;
 import io.jhchoe.familytree.core.family.adapter.out.persistence.FamilyJpaRepository;
+import io.jhchoe.familytree.core.family.domain.Family;
 import io.jhchoe.familytree.docs.AcceptanceTestBase;
 import io.restassured.http.ContentType;
-import java.time.LocalDateTime;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -33,29 +33,19 @@ class FindFamilyControllerTest extends AcceptanceTestBase {
     @Test
     @WithMockOAuth2User
     @DisplayName("가족 정보를 ID로 조회할 수 있다")
-    void find_family_by_id_success() {
+    void given_id_when_find_family_by_then_return_family_with_status_200() {
         // given
-        // 테스트 데이터 설정
-        FamilyEntity savedFamily = familyJpaRepository.save(
-            FamilyEntity.builder()
-                .name("테스트 가족")
-                .description("테스트 가족 설명")
-                .profileUrl("http://test-profile.com/image.jpg")
-                .createdBy(1L)
-                .createdAt(LocalDateTime.now())
-                .modifiedBy(1L)
-                .modifiedAt(LocalDateTime.now())
-                .build()
-        );
+        Family family = Family.newFamily("Family Name", "descrption", "profileUrl");
+        FamilyJpaEntity savedFamily = familyJpaRepository.save(FamilyJpaEntity.from(family));
 
-        // when
+        // when & then
         // API 호출
         FindFamilyResponse response = given()
             .contentType(ContentType.JSON)
             .when()
             .get("/api/families/{id}", savedFamily.getId())
             .then()
-            .statusCode(HttpStatus.OK.value())
+            .statusCode(200)
             .body("id", equalTo(savedFamily.getId().intValue()))
             .body("name", equalTo(savedFamily.getName()))
             .body("description", equalTo(savedFamily.getDescription()))
@@ -82,9 +72,8 @@ class FindFamilyControllerTest extends AcceptanceTestBase {
     @Test
     @WithMockOAuth2User
     @DisplayName("존재하지 않는 가족 ID로 조회할 경우 404 응답을 반환한다")
-    void find_family_by_id_not_found() {
+    void given_non_existent_id_when_find_family_then_return_status_404() {
         // given
-        // 존재하지 않는 ID
         Long nonExistentId = 999L;
 
         // when & then
